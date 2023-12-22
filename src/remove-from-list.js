507806bd5201468020857a1d61390ad5d8e9d701
letanatol <letanatol@gmail.com>
2023-12-22 01:44:28 +0100

    feat: add solution task remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,10 +1,13 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
  * remove all elements from list l that have a value equal to k.
+ * 
+ * дан односвязный список целых чисел l и целое число k,
+ * удалить все элементы из списка l, имеющие значение, равное k.
  *
  * @param {List} l
  * @param {Number} k
@@ -15,6 +18,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be [1, 2, 4, 5]
  *
  * Singly - linked lists are already defined using interface
+ * Отдельно - связанные списки уже определены с помощью интерфейса
  * class ListNode {
  *   constructor(x) {
  *     this.value = x;
@@ -22,9 +26,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  // l = [3, 1, 2, 3, 4, 5] and k = 3 => [1, 2, 4, 5]
+  //      [1, 2, 3, 3, 4, 5] 3 => [1, 2, 4, 5]
+
+  console.log(l.value);
+  if (l.value === k) l = l.next;
+  
+  let nodeTemporal = l.next;
+  while (nodeTemporal) {
+    nodeTemporal.next?.value === k
+      ? nodeTemporal.next = nodeTemporal.next.next
+      : nodeTemporal = nodeTemporal.next;
+  }
+  return l;
 }
 
 module.exports = {
